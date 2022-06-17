d06c4aba54df1313a442f453b7dcea913a9921b3
Ido Aflalo <ido.aflalo@gmail.com>
2022-06-17 23:10:13 +0300

    Fix problem with decorators

--- tests/helpers/TestHelpers.ts
+++ tests/helpers/TestHelpers.ts
@@ -1,5 +1,5 @@
 import { html, LitElement, TemplateResult } from 'lit'
-import { customElement, state } from 'lit/decorators'
+import { customElement, state } from 'lit-element'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type TTemplateResultFunction = (...args: any[]) => TemplateResult
